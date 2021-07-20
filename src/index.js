c9985c916402a3ef94258837c98cd70cc0c2a05d
Gabriel Ayala <gabeayala100@gmail.com>
2021-07-19 22:42:00 -0700

    Add all files and folders.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import $ from 'jquery'; // $ isnt called yet so it is blotted rn
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+/*import any business logic with the following syntax:
+import [BusinessObject] from './triangle.js';*/
+
+
---
+++ src/triangle.js
@@ -0,0 +1,2 @@
+/* export any necessary logic here to the ui using the following syntax:
+export default function [BusinessObject(with any parameters)]*/
