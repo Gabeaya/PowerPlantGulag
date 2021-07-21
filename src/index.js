77a8c35d77fb6e83962dcfccc43be11afb27730d
Gabriel Ayala <gabeayala100@gmail.com>
2021-07-21 11:19:03 -0700

    Comment imports.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import $ from 'jquery'; // $ isnt called yet so it is blotted rn
+/*import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-/*import any business logic with the following syntax:
-import [BusinessObject] from './triangle.js';*/
+import any business logic with the following syntax:
+import [BusinessObject] from './triangle.js';
+for multiple imports from the same file follow this syntax:
+import[{*/
+
 
 
