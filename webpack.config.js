15f7d11569305c762674707ca4421a7b44ab58dd
Gabriel Ayala <gabeayala100@gmail.com>
2021-08-02 17:40:07 -0700

    add the config for dotenv.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -13,12 +14,14 @@ module.exports = {
     contentBase: './dist'      
   },
   plugins: [
+    new Dotenv(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'template-repo', // change title 
       template: './src/index.html',
       inject: 'body'
     })
+    
   ],
   module: {
     rules: [
