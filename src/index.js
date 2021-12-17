9067b6f563cbca5b5d0692c5949da733a5237a50
Gabriel Ayala <gabeayala100@gmail.com>
2021-12-17 12:46:19 -0800

    Separate logic to add soil to a plant and import/export functions.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-/*import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import any business logic with the following syntax:
-import [BusinessObject] from './triangle.js';
-for multiple imports from the same file follow this syntax:
-import[{*/
+import { stateControl, feed, blueFood, hydrate, superWater} from './triangle.js';
+$(document).ready(function() {
 
+// This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
+
+  $('#feed').click(function() {
+    const newState = stateControl(blueFood);
+    $('#soil-value').text(`Soil: ${newState.soil}`);
+  });
+
+// This function doesn't actually do anything useful in this application - it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
+
+  $('#show-state').click(function() {
+    // We just need to call stateControl() without arguments to see our current state.
+    const currentState = stateControl();
+    $('#soil-value').text(`Soil: ${currentState.soil}`);
+  });
+});
 
 
