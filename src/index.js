911b0ed1cd8a5d8553ff1e8eb4b56670eee13cd4
Gabriel Ayala <gabeayala100@gmail.com>
2021-12-19 14:51:54 -0800

    Add functionality to allow buttons to add values to the properties of specific plants.

--- src/index.js
+++ src/index.js
@@ -2,22 +2,61 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { stateControl, feed, blueFood, hydrate, superWater} from './triangle.js';
+import { stateControl, feed, blueFood, hydrate, superWater, heroWater, light, enlighten, plant2} from './triangle.js';
 $(document).ready(function() {
-
-// This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
-
+//the three functions increment the properties of Monty
+  $('#hydrate').click(function() { //Htmls "add H20" button clicked
+    const newState = stateControl(superWater); //we pass our superWater Factory function into our state control function to create object property
+    $('#hydration-value').text(`Hydration: ${newState.water}`);//we apply our new state into the value box to be displayed
+    // note that the water2 in the last part of the line 10 must correspond with the string passed into superWater factory function
+  });
+  // increments a value of 1 to the soil property
   $('#feed').click(function() {
-    const newState = stateControl(blueFood);
+    const newState = stateControl(feed);
     $('#soil-value').text(`Soil: ${newState.soil}`);
   });
-
-// This function doesn't actually do anything useful in this application - it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
+  // increments a value of 5 to the light property
+  $('#light').click(function() {
+    const newState = stateControl(enlighten);
+    $('#light-value').text(`Light: ${newState.light2}`);
+  });
 
   $('#show-state').click(function() {
     // We just need to call stateControl() without arguments to see our current state.
     const currentState = stateControl();
+    $('.monty-values').toggle();
+    $('#hydration-value').text(`Hydration: ${currentState.water}`);
     $('#soil-value').text(`Soil: ${currentState.soil}`);
+    $('#light-value').text(`Light: ${currentState.light2}`);
+    
+  });
+
+// the 3  below affect Darcy's properties
+  $('#2hydrate').click(function() {
+    const newState = plant2(hydrate);
+    $('#2hydration-value').text(`Hydration: ${newState.water}`);
+  });
+  $('#2feed').click(function() {
+    const newState = stateControl(blueFood);
+    $('#2soil-value').text(`Soil: ${newState.soil2}`);
+  });
+  $('#2light').click(function() {
+    const newState = stateControl(light);
+    $('#2light-value').text(`Light: ${newState.light}`);
+  });
+  $('#show-state2').click(function() {
+    // We just need to call stateControl() without arguments to see our current state.
+    const currentState = stateControl();
+    $('.darcy-values').toggle();
+    $('#2hydration-value').text(`Hydration: ${currentState.water}`);
+    $('#2soil-value').text(`Soil: ${currentState.soil2}`);
+    $('#2light-value').text(`Light: ${currentState.light}`);
+  });
+
+// pat the plant's properties
+  $('#3hydrate').click(function() {
+    const newState = stateControl(superWater);
+  $('#3hydration-value').text(`Hydration: ${newState.water}`);
   });
 });
 
